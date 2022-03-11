5254ff676b71ae68eb63714ca1924d64530148f9
Iluas2022 <shorkan92@yandex.ru>
2022-03-11 11:25:05 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'global_theme.dart';
+import 'strings.dart';
+import 'listview_screen.dart';
+
+
+void main() {
+  runApp(const ListViewScreen());
+}
